f1c4e8f11a822c188adb7ed8ce09151bdd15cefc
Wiktoria <wworonecka@gmail.com>
2026-01-30 15:09:23 +0100

    działający szkielet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+// import "./assets/App.scss";
 
 function App() {
   const [username, setUsername] = useState('');
